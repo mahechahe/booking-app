b731b476f528f4cef813d4803448ef9642725cb2
Estiven Mahecha henao <mahechahe@gmail.com>
2022-10-05 16:56:24 -0500

    add booking-app

---
+++ src/components/searchItem/SearchItem.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import "./searchItem.css";
+
+const SearchItem = ({ item }) => {
+  return (
+    <div className="searchItem">
+      <img src={item.photos[0] || 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/214841077.jpg?k=3483a4d8557b29ad6c4c8acea7127e750346194469f6e2944ea22806ee0b48fb&o=&hp=1'} alt="" className="siImg" />
+      <div className="siDesc">
+        <h1 className="siTitle">{item.name}</h1>
+        <span className="siDistance">{item.distance}m from center</span>
+        <span className="siTaxiOp">Free airport taxi</span>
+        <span className="siSubtitle">
+          Studio Apartment with Air conditioning
+        </span>
+        <span className="siFeatures">{item.desc}</span>
+        <span className="siCancelOp">Free cancellation </span>
+        <span className="siCancelOpSubtitle">
+          You can cancel later, so lock in this great price today!
+        </span>
+      </div>
+      <div className="siDetails">
+        {item.rating && <div className="siRating">
+          <span>Excellent</span>
+          <button>{item.rating}</button>
+        </div>}
+        <div className="siDetailTexts">
+          <span className="siPrice">${item.cheapestPrice}</span>
+          <span className="siTaxOp">Includes taxes and fees</span>
+          <Link to={`/hotels/${item._id}`}>
+          <button className="siCheckButton">See availability</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchItem;
